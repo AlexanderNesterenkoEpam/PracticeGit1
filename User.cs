4d9b63cd7d7dce3852fd77570b4a5eaf4630b1ea
Aliaksandr Nestsiarenka1 <Aliaksandr_Nestsiarenka1@epam.com>
2019-10-18 19:55:02 +0300

    task_3 Добавление метода PrintUserName""
    This reverts commit 727347d2795b763a8f2652575c46ecc8de69f830.

--- User.cs
+++ User.cs
@@ -8,10 +8,16 @@
 
         public string LastName { get; set; }
 
-        public User(string name, string age)
+        public User(string name, string age, string lastName)
         {
             this.Age = age;
             this.Name = name;
+            this.LastName = lastName;
         }
+
+        public void PrintUserName()
+        {
+            System.Console.WriteLine("User name: " + this.Name + " " + this.LastName + "!");
+        }    
     }
 }
