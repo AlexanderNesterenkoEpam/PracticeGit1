2d1635dc49fb3801d9870cd46a541ad9f9f64282
Aliaksandr Nestsiarenka1 <Aliaksandr_Nestsiarenka1@epam.com>
2019-10-18 19:37:08 +0300

    #task-1 Добавление класса юзер
    Добавление класса юзер
    Предназначен  для важных вещей.

---
+++ User.cs
@@ -0,0 +1,15 @@
+﻿namespace TestGitProject
+{
+    public class User
+    {
+        public string Age { get; set; }
+
+        public string Name { get; set; }
+
+        public User(string name, string age)
+        {
+            this.Age = age;
+            this.Name = name;
+        }
+    }
+}
