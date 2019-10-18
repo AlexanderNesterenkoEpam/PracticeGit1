5af7c50ae9f1f47a0828bc81467d93215f118289
Aliaksandr Nestsiarenka1 <Aliaksandr_Nestsiarenka1@epam.com>
2019-10-18 19:48:54 +0300

    task_3 Добавление метода PrintUserName
    Добавление метода в PrintUserName в
    класс User
    Метод служит для выведение имени
    пользователя на экран

---
+++ Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGitProject
+{
+    public class Car
+    {
+        public string Model { get; set; }
+
+        public string Title { get; set; }
+
+        public Car(string model, string title)
+        {
+            this.Model = model;
+            this.Title = title;
+        }
+    }
+}
