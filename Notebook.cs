6a9908141590d9da0299648884cdb4ad7fd2d503
SecondUser <SecondUser@mail.com>
2019-10-22 15:50:23 +0300

    #TBD-6 Добавление поля Title классу Notebook и добавление конструктора с этим параметром
    Добавление поля Title для дальнейшего создания
    объектов класса с полем title

--- Notebook.cs
+++ Notebook.cs
@@ -2,5 +2,11 @@
 {
     public class Notebook
     {
+        public string Title { get; set; }
+
+        public Notebook(string title)
+        {
+            this.Title = title;
+        }
     }
 }
